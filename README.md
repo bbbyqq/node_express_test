1265b0577c2db9d2848b9c9907d093dca6760790
bbbyqq <2287532812@qq.com>
2022-04-22 14:48:37 +0800

    new

---
+++ README.md
@@ -0,0 +1,11 @@
+### 项目启动
+
+- 安装依赖
+```
+npm install
+```
+
+- 启动命令
+```
+npm run dev 或者 node api.js
+```
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8")
+    next()
+})
+
+
+const questions = [
+    {
+        data: 213,
+        num: 444,
+        age: 12
+    },
+    {
+        data: 456,
+        num: 678,
+        age: 13
+    }]
+
+
+//写个接口123
+app.get('/123', function (req, res) {
+    res.status(200)
+    res.json(questions)
+});
+
+//配置服务端口
+const server = app.listen(3000, function () {
+    // const host = server.address().address
+    const port = server.address().port
+    console.log('Example app listening at http://%s:%s/%s', 'localhost', port, '123')
+})
+
